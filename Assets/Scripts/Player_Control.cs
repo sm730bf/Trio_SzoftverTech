8829c0665b286612096a6558f6206b287f8bb659
Meszaros_Istvan_WZZTLL <mesziist@gmail.com>
2026-03-22 22:55:31 +0100

    Added a few Animations
    Idle & Run Animation added

--- Assets/Scripts/Player_Control.cs
+++ Assets/Scripts/Player_Control.cs
@@ -4,21 +4,36 @@ using UnityEngine.InputSystem;
 public class Player_Control : MonoBehaviour
 {
     Rigidbody2D playerBody;
+    Animator playerAnim;
     [SerializeField] private float speed;
     [SerializeField] private float jump;
     private void Awake()
     {
         playerBody = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponentInChildren<Animator>();
+
     }
 
     // Update is called once per frame
     private void Update()
     {
-     playerBody.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerBody.linearVelocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        playerBody.linearVelocity = new Vector2(horizontalInput * speed, playerBody.linearVelocity.y);
 
-    if(Input.GetKey(KeyCode.Space))
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
         {
             playerBody.linearVelocity = new Vector2(playerBody.linearVelocity.x, jump);
         }
+
+        playerAnim.SetBool("Run", horizontalInput != 0);
     }
 }
